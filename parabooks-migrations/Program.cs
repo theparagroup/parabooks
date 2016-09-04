5af71ed3e44f8e63056154e3e31fe0867807052b
Joe Barnett <joebarn@users.noreply.github.com>
2016-09-03 21:08:40 -0400

    ''

---
+++ parabooks-migrations/Program.cs
@@ -0,0 +1,17 @@
+﻿using com.paralib.Migrations.Runner;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.theparagroup.parabooks.migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandProcessor.ConsoleMain(args);
+        }
+    }
+}
---
+++ parabooks-migrations/VersionTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator.VersionTableInfo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.theparagroup.parabooks.migrations
+{
+    [VersionTableMetaData]
+    public class VersionTable : DefaultVersionTableMetaData
+    {
+        public override string TableName
+        {
+            get { return "parabooks_version_info"; }
+        }
+    }
+}
